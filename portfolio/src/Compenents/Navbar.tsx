79021e649e0eb645db9abe5621bcbadacf909eba
z1n0u <taztala1@gmail.com>
2024-10-25 20:44:32 +0100

    update portfolio

--- portfolio/src/Compenents/Navbar.tsx
+++ portfolio/src/Compenents/Navbar.tsx
@@ -1,14 +1,28 @@
-
-
-export default function App () {
+export default function App() {
   return (
-    <nav className='fixed top-28 -right-7 justify-center items-center z-10 -rotate-90 text-right hidden md:flex'>
-        <a href="#about" className='text-white mr-5 cursor-pointer text-lg opacity-50 hover:opacity-100 transition-all'>About</a>
-        <a href="#work" className='text-white mr-5 cursor-pointer text-lg opacity-50 hover:opacity-100 transition-all'>Work</a>
-        <a href="/" className='flex items-center justify-center cursor-pointer'>
-            <span className='bg-white rounded-full inline-block w-4 h-4'></span>
-        </a>
+    <nav className="fixed top-28 -right-7 justify-center items-center z-10 -rotate-90 text-right hidden md:flex">
+      <a
+        href="TAREK BOURENNANE CV.pdf"
+        download="Tarek_BOURENNANE_CV.pdf"
+        className="text-white mr-5 cursor-pointer text-lg opacity-50 hover:opacity-100 transition-all"
+      >
+        CV
+      </a>
+      <a
+        href="#about"
+        className="text-white mr-5 cursor-pointer text-lg opacity-50 hover:opacity-100 transition-all"
+      >
+        About
+      </a>
+      <a
+        href="#work"
+        className="text-white mr-5 cursor-pointer text-lg opacity-50 hover:opacity-100 transition-all"
+      >
+        Work
+      </a>
+      <a href="/" className="flex items-center justify-center cursor-pointer">
+        <span className="bg-white rounded-full inline-block w-4 h-4"></span>
+      </a>
     </nav>
   );
 }
-
--- portfolio/src/sections/Work.tsx
+++ portfolio/src/sections/Work.tsx
@@ -1,100 +1,203 @@
-import {  motion } from "framer-motion";
+import { motion } from "framer-motion";
 
+export function Work() {
+  const randomInteger = Math.floor(Math.random() * (150 - 30 + 1)) + 30;
 
-export function Work () {
-    const randomInteger = Math.floor(Math.random() * (150 - 30 + 1)) + 30;
-
-    const variantforbar = {
-        hidden: {
-          width: "0",
-        },
-        visible: {
-            width :randomInteger +"px",
-            opacity:1
-        },
-    }
-    const variantforsmalltest = {
-        hidden: {
-          opacity: 0.5,
-        },
-        visible: {
-            opacity:1
-        },
-    }
-    const variantforshowcaseDiv = {
-      hidden: {
-        x: '100vw', 
-        opacity:0,
-      },
-      visible: {
-        x: '0', 
-        rotateY: 0,
-        rotateZ: -5,
-        opacity:1,
-      },
-  }
+  const variantforbar = {
+    hidden: {
+      width: "0",
+    },
+    visible: {
+      width: randomInteger + "px",
+      opacity: 1,
+    },
+  };
+  const variantforsmalltest = {
+    hidden: {
+      opacity: 0.5,
+    },
+    visible: {
+      opacity: 1,
+    },
+  };
+  const variantforshowcaseDiv = {
+    hidden: {
+      x: "100vw",
+      opacity: 0,
+    },
+    visible: {
+      x: "0",
+      rotateY: 0,
+      rotateZ: -5,
+      opacity: 1,
+    },
+  };
   return (
     <div id="work" className="relative w-[80%] left-24">
       <h3 className="text-white flex items-center text-2xl font-bold">
-        <span className="bg-gradient-to-r from-amber-200 to-yellow-500 w-12 h-3 mr-4"/>
+        <span className="bg-gradient-to-r from-amber-200 to-yellow-500 w-12 h-3 mr-4" />
         WORK
       </h3>
       <ul className="pt-4 pb-4 relative">
         <li>
-            <motion.a  target="_blank" className="flex items-center gap-3" initial="hidden" whileHover="visible">
-                <motion.span variants={variantforbar} className="h-4 bg-gradient-to-r from-fuchsia-500 to-pink-500"></motion.span>
-                <span className="text-lg text-white test">Incident Management App</span>
-                <span className="bg-gradient-to-r from-red-500 to-orange-500 rounded-full inline-block w-2 h-2 md:h-3 md:w-3 lg:h-4 lg:w-4 "></span>
-                <motion.span variants={variantforsmalltest} className="text-white opacity-50 hover:opacity-100">For BNA (bank)</motion.span>
-                
-                <motion.div variants={variantforshowcaseDiv} transition={{  duration: 1, ease: "easeInOut"}} className="flex bg-black w-96 h-44 absolute top-0 right-[5%]">
-                  <div className="relative flex w-full h-full">
-                    <img src="https://utfs.io/f/e223f295-415f-42be-b330-59d8de615d5f-keb9xm.png" alt=""  className="w-full h-full  object-cover"/>
-                    <div className="flex absolute -top-2 -left-6 h-2 md:h-3 lg:h-4 w-14 bg-gradient-to-r from-emerald-400 to-cyan-400"></div>
-                    <div className="flex absolute -bottom-2 -right-5 h-2 md:h-3 lg:h-4 w-20 bg-gradient-to-r from-amber-200 to-yellow-500"></div>
-                  </div>
-                </motion.div>
-            </motion.a>
+          <motion.a
+            target="_blank"
+            className="flex items-center gap-3"
+            initial="hidden"
+            whileHover="visible"
+            href="https://dorodz.com"
+          >
+            <motion.span
+              variants={variantforbar}
+              className="h-4 bg-gradient-to-r from-fuchsia-500 to-pink-500"
+            ></motion.span>
+            <span className="text-lg text-white test">Doro App</span>
+            <span className="bg-gradient-to-r from-red-500 to-orange-500 rounded-full inline-block w-2 h-2 md:h-3 md:w-3 lg:h-4 lg:w-4 "></span>
+            <motion.span
+              variants={variantforsmalltest}
+              className="text-white opacity-50 hover:opacity-100"
+            >
+              a donations systeme for Algerian streamers
+            </motion.span>
+
+            <motion.div
+              variants={variantforshowcaseDiv}
+              transition={{ duration: 1, ease: "easeInOut" }}
+              className="flex bg-black w-96 h-44 absolute top-0 right-[5%]"
+            >
+              <div className="relative flex w-full h-full">
+                <img
+                  src="/doro.jpeg"
+                  alt=""
+                  className="w-full h-full  object-cover"
+                />
+                <div className="flex absolute -top-2 -left-6 h-2 md:h-3 lg:h-4 w-14 bg-gradient-to-r from-emerald-400 to-cyan-400"></div>
+                <div className="flex absolute -bottom-2 -right-5 h-2 md:h-3 lg:h-4 w-20 bg-gradient-to-r from-amber-200 to-yellow-500"></div>
+              </div>
+            </motion.div>
+          </motion.a>
+        </li>
+        <li>
+          <motion.a
+            target="_blank"
+            className="flex items-center gap-3"
+            initial="hidden"
+            whileHover="visible"
+          >
+            <motion.span
+              variants={variantforbar}
+              className="h-4 bg-gradient-to-r from-fuchsia-500 to-pink-500"
+            ></motion.span>
+            <span className="text-lg text-white test">
+              Incident Management App
+            </span>
+            <span className="bg-gradient-to-r from-red-500 to-orange-500 rounded-full inline-block w-2 h-2 md:h-3 md:w-3 lg:h-4 lg:w-4 "></span>
+            <motion.span
+              variants={variantforsmalltest}
+              className="text-white opacity-50 hover:opacity-100"
+            >
+              For BNA (bank)
+            </motion.span>
+
+            <motion.div
+              variants={variantforshowcaseDiv}
+              transition={{ duration: 1, ease: "easeInOut" }}
+              className="flex bg-black w-96 h-44 absolute top-0 right-[5%]"
+            >
+              <div className="relative flex w-full h-full">
+                <img
+                  src="/ListeRapportAgent.png"
+                  alt=""
+                  className="w-full h-full  object-cover"
+                />
+                <div className="flex absolute -top-2 -left-6 h-2 md:h-3 lg:h-4 w-14 bg-gradient-to-r from-emerald-400 to-cyan-400"></div>
+                <div className="flex absolute -bottom-2 -right-5 h-2 md:h-3 lg:h-4 w-20 bg-gradient-to-r from-amber-200 to-yellow-500"></div>
+              </div>
+            </motion.div>
+          </motion.a>
         </li>
       </ul>
       <h3 className="text-white flex items-center text-2xl font-bold">
-        <span className="bg-gradient-to-r from-emerald-400 to-cyan-400 w-12 h-3 mr-4"/>
+        <span className="bg-gradient-to-r from-emerald-400 to-cyan-400 w-12 h-3 mr-4" />
         Side Projects
       </h3>
       <ul className="pt-4 pb-4 relative">
-      <li>
-        <motion.a href="https://lugx-ten.vercel.app" target="_blank" className="flex items-center gap-3" initial="hidden" whileHover="visible">
-                <motion.span variants={variantforbar} className="h-4 bg-gradient-to-r from-fuchsia-500 to-pink-500"></motion.span>
-                <span className="text-lg text-white test">Lugx</span>
-                <span className="bg-gradient-to-r from-red-500 to-orange-500 rounded-full inline-block w-2 h-2 md:h-3 md:w-3 lg:h-4 lg:w-4 "></span>
-                <motion.span variants={variantforsmalltest} className="text-white opacity-50 hover:opacity-100">Games selling websites</motion.span>
-                
-                <motion.div variants={variantforshowcaseDiv} transition={{  duration: 1, ease: "easeInOut"}} className="flex bg-black w-96 h-44 absolute top-0 right-[5%]">
-                  <div className="relative flex w-full h-full">
-                    <img src="https://utfs.io/f/905cea6f-e013-42da-bd1a-40931d10c7df-1zfwa.png" alt=""  className="w-full h-full  object-cover"/>
-                    <div className="flex absolute -top-2 -left-6 h-2 md:h-3 lg:h-4 w-14 bg-gradient-to-r from-emerald-400 to-cyan-400"></div>
-                    <div className="flex absolute -bottom-2 -right-5 h-2 md:h-3 lg:h-4 w-20 bg-gradient-to-r from-amber-200 to-yellow-500"></div>
-                  </div>
-                </motion.div>
-            </motion.a>
+        <li>
+          <motion.a
+            href="https://lugx-ten.vercel.app"
+            target="_blank"
+            className="flex items-center gap-3"
+            initial="hidden"
+            whileHover="visible"
+          >
+            <motion.span
+              variants={variantforbar}
+              className="h-4 bg-gradient-to-r from-fuchsia-500 to-pink-500"
+            ></motion.span>
+            <span className="text-lg text-white test">Lugx</span>
+            <span className="bg-gradient-to-r from-red-500 to-orange-500 rounded-full inline-block w-2 h-2 md:h-3 md:w-3 lg:h-4 lg:w-4 "></span>
+            <motion.span
+              variants={variantforsmalltest}
+              className="text-white opacity-50 hover:opacity-100"
+            >
+              Games selling websites
+            </motion.span>
+
+            <motion.div
+              variants={variantforshowcaseDiv}
+              transition={{ duration: 1, ease: "easeInOut" }}
+              className="flex bg-black w-96 h-44 absolute top-0 right-[5%]"
+            >
+              <div className="relative flex w-full h-full">
+                <img
+                  src="/lugx.png"
+                  alt=""
+                  className="w-full h-full  object-cover"
+                />
+                <div className="flex absolute -top-2 -left-6 h-2 md:h-3 lg:h-4 w-14 bg-gradient-to-r from-emerald-400 to-cyan-400"></div>
+                <div className="flex absolute -bottom-2 -right-5 h-2 md:h-3 lg:h-4 w-20 bg-gradient-to-r from-amber-200 to-yellow-500"></div>
+              </div>
+            </motion.div>
+          </motion.a>
         </li>
         <li>
-        <motion.a href="https://z1n0u.github.io/E-commerce-sneakers/" target="_blank" className="flex items-center gap-3" initial="hidden" whileHover="visible">
-                <motion.span variants={variantforbar} className="h-4 bg-gradient-to-r from-fuchsia-500 to-pink-500"></motion.span>
-                <span className="text-lg text-white test">.Sneakers</span>
-                <span className="bg-gradient-to-r from-red-500 to-orange-500 rounded-full inline-block w-2 h-2 md:h-3 md:w-3 lg:h-4 lg:w-4 "></span>
-                <motion.span variants={variantforsmalltest} className="text-white opacity-50 hover:opacity-100">E-commers site for sneakers</motion.span>
-                
-                <motion.div variants={variantforshowcaseDiv} transition={{  duration: 1, ease: "easeInOut"}} className="flex bg-black w-96 h-44 absolute top-0 right-[5%]">
-                  <div className="relative flex w-full h-full">
-                    <img src="https://utfs.io/f/6ce0a29c-b222-4854-9c21-e85ee79c8a78-fk2gdc.jpeg" alt=""  className="w-full h-full  object-cover"/>
-                    <div className="flex absolute -top-2 -left-6 h-2 md:h-3 lg:h-4 w-14 bg-gradient-to-r from-emerald-400 to-cyan-400"></div>
-                    <div className="flex absolute -bottom-2 -right-5 h-2 md:h-3 lg:h-4 w-20 bg-gradient-to-r from-amber-200 to-yellow-500"></div>
-                  </div>
-                </motion.div>
-            </motion.a>
+          <motion.a
+            href="https://z1n0u.github.io/E-commerce-sneakers/"
+            target="_blank"
+            className="flex items-center gap-3"
+            initial="hidden"
+            whileHover="visible"
+          >
+            <motion.span
+              variants={variantforbar}
+              className="h-4 bg-gradient-to-r from-fuchsia-500 to-pink-500"
+            ></motion.span>
+            <span className="text-lg text-white test">.Sneakers</span>
+            <span className="bg-gradient-to-r from-red-500 to-orange-500 rounded-full inline-block w-2 h-2 md:h-3 md:w-3 lg:h-4 lg:w-4 "></span>
+            <motion.span
+              variants={variantforsmalltest}
+              className="text-white opacity-50 hover:opacity-100"
+            >
+              E-commers site for sneakers
+            </motion.span>
+
+            <motion.div
+              variants={variantforshowcaseDiv}
+              transition={{ duration: 1, ease: "easeInOut" }}
+              className="flex bg-black w-96 h-44 absolute top-0 right-[5%]"
+            >
+              <div className="relative flex w-full h-full">
+                <img
+                  src="/sneakers2.jpeg"
+                  alt=""
+                  className="w-full h-full  object-cover"
+                />
+                <div className="flex absolute -top-2 -left-6 h-2 md:h-3 lg:h-4 w-14 bg-gradient-to-r from-emerald-400 to-cyan-400"></div>
+                <div className="flex absolute -bottom-2 -right-5 h-2 md:h-3 lg:h-4 w-20 bg-gradient-to-r from-amber-200 to-yellow-500"></div>
+              </div>
+            </motion.div>
+          </motion.a>
         </li>
-       
       </ul>
       <div className="w-[1px] h-[99%] bg-white absolute opacity-30 -left-6 top-0 after:opacity-95 after:absolute after:bottom-0 after:-left-2 after:h-0 after:w-0 after:border-x-8 after:border-x-transparent after:border-t-[9px]"></div>
     </div>
